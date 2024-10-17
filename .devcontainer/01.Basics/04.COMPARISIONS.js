eb6bdd163ca7df76813900dbbcde1e3071b44a54
Aman Singh <avi102710@gmail.com>
2024-10-17 23:11:49 +0000

    comparisions

---
+++ .devcontainer/01.Basics/04.COMPARISIONS.js
@@ -0,0 +1,31 @@
+// console.log( 2 > 1);
+// console.log( 2 < 1);
+// console.log( 2 >= 1);
+// console.log( 2 <=1 );
+// console.log( 2==1);
+// console.log( 2 != 1);
+
+//    console.log("2" > 1);
+//    console.log("02" >1 );
+
+
+// comparsion like null and unidentified also makes confusion, avoid these types of comparision only clean code we have to use.
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log( null >= 0);
+
+// The reason is that an equality check == and comparision ><>=<= work differently
+// comparision convert null to a number ,treating it as a 0. Thats why (3) null >= 0 is true and (1) null is false.
+
+// console.log(undefined == 0);
+
+// console.log(undefined > 0);
+
+// console.log(undefined > 0);
+
+// === it's called strict check triple equal
+console.log("2" === 2 );
+
+
+
