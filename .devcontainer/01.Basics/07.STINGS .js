cc41bdb6480a35852ae6bc0676f49283ae388679
Aman Singh <avi102710@gmail.com>
2024-10-18 21:02:10 +0000

    STINGS

---
+++ .devcontainer/01.Basics/07.STINGS .js
@@ -0,0 +1,26 @@
+//const name ="Aman Singh"
+//const repoCount = 27
+ // console.log(name + repoCount + " Value"); its a outdated style
+
+
+
+ const name ="Aman Singh"
+ const repoCount = 27
+
+console.log('My name is ${name} and my repo count is ${repoCount} ');
+
+
+const gameName = new String ('Aman')
+console.log(gameName [0]);    // it prints only 1st letter of Aman A ,[1]m ,[2]a,[3]n .
+console.log(gameName.__proto__); // (.dot __ two underscore proto__two underscore)
+
+console.log(gameName.length); // .length :- it counts total no. of words like Aman has 4.
+console.log(gameName.toUpperCase()); // .toUpperCase()  ;-it writes same value here Aman
+console.log(gameName.charAt(3)); // output will be n :- Aman 0123 (no. to alphabet)
+console.log(gameName.indexOf('m'));// m=1 (alphabet to no.)
+
+
+
+
+
+
