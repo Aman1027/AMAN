b7a6c4ff777ca7e41d82550689c682e6d30f6b12
Aman Singh <avi102710@gmail.com>
2024-10-20 15:48:37 +0000

    dates in js

--- .devcontainer/01.Basics/08.DATES IN JS.js
+++ .devcontainer/01.Basics/08.DATES IN JS.js
@@ -23,6 +23,35 @@
 
  let myCreatedDate4 = new Date ("10-20-2024")
  console.log(myCreatedDate4.toLocaleString());
+
+
+ let myTimeStamp = Date.now()
+ console.log(myTimeStamp);
+ console.log(myCreatedDate.getTime());
+
+ console.log(Math.floor(Date.now()/1000)); // fpr converting milisecond in seond we have to divide it by 1000.
+
+ 
+ let newDate =new Date()
+ console.log(newDate);
+ console.log(newDate.getMonth()); // month starts from zero so if u want to show real months then add +1 in the code
+ console.log(newDate.getMonth()+1);
+ console.log(newDate.getDate());
+ console.log(newDate.getDay()); // sunday is take as zero 0
+
+
+ //' ${ newDate.getDay()} and the Time'
+
+ newDate.toLocaleString('default',{weekday:"long"})
+ console.log(newDate.toLocaleString());
+ 
+ newDate.toLocaleString('default',{calendar:"getMonth"})
+    console.log(newDate.toLocaleString());
+ 
+  
+  
+ 
+ 
  
  
  
