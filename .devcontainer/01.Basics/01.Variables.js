61e0fdc75b5e29a327d03037c83c0760be519c31
Aman Singh <avi102710@gmail.com>
2024-10-21 16:50:03 +0000

    variables

--- .devcontainer/01.Basics/01.Variables.js
+++ .devcontainer/01.Basics/01.Variables.js
@@ -7,7 +7,7 @@ let accountState; /*it will show undefined bcz we didnt given the value)*/
 /* comment section written as this way
 constant written as const we can not change the value of const
 variable written as var and let. now we use let and we can change the value of let any time.
-prefer not to use var bca of issue in block scope and functional scope.
+prefer not to use var because of issue in block scope and functional scope.
 */
 //accountID=27// not possible new bcz it is in the constant side already.
 accountEmail="avi@gmail.com"
