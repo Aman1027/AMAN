f83d54911a7949b37a23cf540f9a5e752b1dcfd5
Aman Singh <avi102710@gmail.com>
2024-10-23 18:27:19 +0000

    Array

--- .devcontainer/01.Basics/02.BASICS.js/02.Array.js
+++ .devcontainer/01.Basics/02.BASICS.js/02.Array.js
@@ -7,7 +7,7 @@ const dc_heros= ["Superman","Flash","Batman"]
                   //till array 3 it is marvel heros and in array 4 all 3 dc heros
 
 
-// const allHeros =marvel_heros.concat(dc_heros) // it combined all heros together
+// const allHeros =marvel_heros.concat(dc_heros) //  .it  gives new array by combing more array.      it combined all heros together
 // console.log(allHeros); 
 
 
@@ -17,12 +17,12 @@ const dc_heros= ["Superman","Flash","Batman"]
 // console.log(all_new_heros);
 
 
-// const another_array =[1,2,3,[5,4,6,[8,9,10,27]]]
-// const real_another_array = another_array.flat(Infinity)
-// console.log(real_another_array);
+const another_array =[1,2,3,[5,4,6,[8,9,10,27]]]
+const real_another_array = another_array.flat(Infinity) /// it gives all array in a single together
+console.log(real_another_array);
 
 console.log(Array.isArray("Aman"));
-console.log(Array.from("Aman"));
+console.log(Array.from("Aman")); // it spreadout Aman as 'A','m','a','n;
 
 console.log(Array.from({name:"Aman"})); // interesting it gives blank result
 
--- .devcontainer/01.Basics/02.BASICS.js/tempCodeRunnerFile.js
+++ .devcontainer/01.Basics/02.BASICS.js/tempCodeRunnerFile.js
@@ -1,43 +1,4 @@
- // array
 
-
- const myArr = [ 0,1,2,3,4,5,]
- console.log(myArr[0]); // for only 1 value but if we dont put any value it prints all value inside the array
- console.log(myArr);
-
- const myHeros =["HANUMAN","SHAKTIMAN"]
-console.log(myHeros);
- 
-const myArr2 =  new Array(1 ,2,3,4)
-console.log(myArr2);
-
- 
-/////// Array METHODS:-
-
-myArr.push(6)  //  whatever we write the value in .push() it gives the value with all previous and this value together .previous array 1 ,2,3,4,5 and its 6 print together
-myArr.push(10)
-console.log(myArr);
-
-
-myArr.push(27) // if value outside the array value then it gives -1 as a result.
-//    myArr.pop()  // .pop() it is used to remove the last value of array like here we have 27 but after .pop() ,its removed
-
-
-myArr.unshift(9)  // .unshift is used to make a new value sbse phle sb value ko adjust hona pdta h thats why we dont used much
-//myArr.shift()  // it arrange the value by removing the .unshift
-
-
-console.log(myArr.includes(10)); // .includes tell that the no. is in the array or not if it is then TRUE , if its not then FALSE
-console.log(myArr.includes(7));
-console.log(myArr.includes(5));
-
-
-console.log(myArr.indexOf(10)); // .index :- position teller (if we write the value within the array given then its counts the position from zero to that  number position .here 10 is at 8th position
-console.log(myArr.indexOf(27)); // if the value is outside the array then it always shows -1 value
-
-
-const newArr = myArr.join()
-console.log(myArr);  // it also gives all array value
-console.log(newArr); // it gives all array value
-console.log(typeof newArr);
-
+const another_array =[1,2,3,[5,4,6,[8,9,10,27]]]
+const real_another_array = another_array.flat(Infinity)
+console.log(real_another_array);
